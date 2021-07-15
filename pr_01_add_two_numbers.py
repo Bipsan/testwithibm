7cbd7288134f64d80d12d8843af68c1c856db922
Bipsan <76049721+Bipsan@users.noreply.github.com>
2021-07-15 18:39:15 +0545

    Add files via upload
    Adding two numbers

---
+++ pr_01_add_two_numbers.py
@@ -0,0 +1,6 @@
+a = input("enter your first number")
+a = int(a)
+b= input("enter your second number")
+b = int(b)
+# print("the sum of your numbers is:", a+b ) #this is first problem
+print("the remainder when first number is divided by second is:", a%b ) #this is second
